43879dac616bf082041a6df17256de4cc5b8522f
Richard Barker <richbarker@gmail.com>
2022-12-04 18:04:39 +1100

    Day 4 part 2 solution

--- src/advent_of_code_2022/day4.clj
+++ src/advent_of_code_2022/day4.clj
@@ -2,34 +2,36 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2022.core :refer :all]))
 
-(defn parse-pair [p] (apply range (map parse-int (clojure.string/split p #"-"))))
+(defn parse-pair [p]
+  (let [[a b] (map parse-int (clojure.string/split p #"-"))]
+    (range a (inc b))))
 
-(defn count-contained-pairs [input-file]
-  (let [pairs           (clojure.string/split-lines (slurp input-file))
-        parse-pair      (fn [p]
-                          (map parse-int (clojure.string/split p #"-")))
+(def fully-contains (fn [p-large p-small]
+                      (every? (set p-large) p-small)))
 
-        fully-contains  (fn [p-large p-small]
-                          (and
-                            (<= (first p-large) (first p-small))
-                            (<= (second p-small) (second p-large))))
+(def overlaps? (fn [p1 p2]
+                 (some (set p1) p2)))
 
-        fully-contained (fn [p]
-                          (let [p (map parse-pair (clojure.string/split p #","))
-                                p (sort-by (fn [[x y]] (- y x))
-                                           p)
-                                [p-small p-large] p]
+(def fully-contained (fn [p match-fn]
+                       (let [p (map parse-pair (clojure.string/split p #","))
+                             p (sort-by count p)
+                             [p-small p-large] p]
 
-                            (fully-contains p-large p-small)))]
+                         (match-fn p-large p-small))))
+
+(defn count-contained-pairs [input-file match-fn]
+  (let [pairs (clojure.string/split-lines (slurp input-file))]
 
     (reduce (fn [c p]
-              (if (fully-contained p)
+              (if (fully-contained p match-fn)
                 (inc c)
                 c))
             0
             pairs)))
 
-(deftest part1-testing
-
-  (is (= 2 (count-contained-pairs "resources/day4-sample.txt")))
-  (is (= 562 (count-contained-pairs "resources/day4.txt"))))
+(deftest pair-testing
+  (is (= [3] (parse-pair "3-3")))
+  (is (= 2 (count-contained-pairs "resources/day4-sample.txt" fully-contains)))
+  (is (= 4 (count-contained-pairs "resources/day4-sample.txt" overlaps?)))
+  (is (= 562 (count-contained-pairs "resources/day4.txt" fully-contains)))
+  (is (= 924 (count-contained-pairs "resources/day4.txt" overlaps?))))
