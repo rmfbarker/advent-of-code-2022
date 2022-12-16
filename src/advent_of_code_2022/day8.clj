f2b4fec05199773806bc94f01151fa23eefc5d22
Richard Barker <richbarker@gmail.com>
2022-12-16 21:46:14 +1100

    Day 8 Part 2

--- src/advent_of_code_2022/day8.clj
+++ src/advent_of_code_2022/day8.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2022.core :refer :all]))
 
-(def input-file "resources/day8-sample.txt")
+;(def input-file "resources/day8-sample.txt")
 (def input-file "resources/day8.txt")
 
 (def input (mapv #(mapv parse-int %)
@@ -34,22 +34,36 @@
                         col-up      (take y (get-col x))
                         col-down    (drop (inc y) (get-col x))
 
-                        is-shorter? #(< % tree)
-                        ]]
-              (do
-                ;(println [x y] tree "row left" row-left "visible from  left " (every? is-shorter? row-left))
-                ;
-                ;(println [x y] tree "row right" row-right "visible from right " (every? is-shorter? row-right))
-                ;;
-                ;(println [x y] "col up" col-up "visible from top" (every? is-shorter? col-up))
-                ;;
-                ;(println [x y] "col down" col-down "visible from bottom" (every? is-shorter? col-down))
-                ;[x y]
-
-                (or
-                  (every? is-shorter? row-left)
-                  (every? is-shorter? row-right)
-                  (every? is-shorter? col-up)
-                  (every? is-shorter? col-down)))))))
+                        is-shorter? #(< % tree)]]
+              (or
+                (every? is-shorter? row-left)
+                (every? is-shorter? row-right)
+                (every? is-shorter? col-up)
+                (every? is-shorter? col-down))))))
 
 (deftest part-1-test (is (= 1688 visible-trees)))
+
+
+(def part2
+  (apply max
+         (for [x (range x-length) y (range y-length)
+               :let [tree          (get-tree x y)
+
+                     row-left      (take x (get input y))
+                     row-right     (drop (inc x) (get input y))
+                     col-up        (take y (get-col x))
+                     col-down      (drop (inc y) (get-col x))
+
+                     trees-visible (fn [trees] (reduce (fn [acc t]
+                                                         (if (<= tree t)
+                                                           (reduced (inc acc))
+                                                           (inc acc))) 0 trees))
+
+                     trees-left    (trees-visible (reverse row-left))
+                     trees-right   (trees-visible row-right)
+                     trees-up      (trees-visible (reverse col-up))
+                     trees-down    (trees-visible col-down)
+                     ]]
+           (* trees-left trees-right trees-up trees-down))))
+
+(deftest part-1-test (is (= 410400 part2)))
