c057a663c6b44126d6c8cf492ddbc9eedad7fac8
Richard Barker <richbarker@gmail.com>
2022-12-24 21:06:59 +1300

    Day 10 Part 2

--- src/advent_of_code_2022/day10.clj
+++ src/advent_of_code_2022/day10.clj
@@ -5,17 +5,32 @@
 (def sample-file "resources/day10-sample.txt")
 (def input-file "resources/day10.txt")
 
+(defn parse-add-instr [instr]
+  (-> instr
+      (clojure.string/split #" ")
+      second
+      parse-int))
+
+(defn do-addx [states instr]
+  (let [curr (peek states)
+        addx (parse-add-instr instr)]
+    (conj states
+          (update curr :cycle inc)
+          (-> curr
+              (update :cycle + 2)
+              (update :register + addx)))))
+
+(defn do-noop [states]
+  (let [curr (peek states)
+        next (update curr :cycle inc)]
+    (conj states next)))
+
 (defn cycle-states [file-path]
   (reduce
     (fn [states instr]
-      (let [curr (peek states)]
-        (conj states
-              (if (= instr "noop")
-                (update curr :cycle inc)
-                (let [addx (parse-int (second (clojure.string/split instr #" ")))]
-                  (-> curr
-                      (update :cycle + 2)
-                      (update :register + addx)))))))
+      (if (= instr "noop")
+        (do-noop states)
+        (do-addx states instr)))
     [{:cycle 0 :register 1}]
     (parse-lines file-path)))
 
@@ -39,3 +54,20 @@
   (is (= 13140 (part-1 sample-file)))
   (is (= 14040 (part-1 input-file)))
   )
+
+(defn part-2 [file-path]
+  (let [states (cycle-states file-path)]
+   (loop [cycle 1]
+     (when (< cycle (count states))
+       (let [start-state (nth states (dec cycle))
+             end-state   (nth states cycle)
+             reg         (:register start-state)]
+         #_(println "cycle" cycle "cursor pos" (dec cycle) "at start" start-state "at end" end-state
+                    "crt" (if (<= reg cycle (+ 2 reg)) "#" "."))
+         (print (if (<= reg (mod cycle 40) (+ 2 reg)) "#" "."))
+         (if (zero? (mod cycle 40)) (println))
+         (recur (inc cycle)))))))
+
+(comment
+  (part-2 sample-file)
+  (part-2 input-file))
